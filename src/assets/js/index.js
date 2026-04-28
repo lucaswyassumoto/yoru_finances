0fb4560c8bc1b77a0927e4e893f32e73250525cc
lucaswyassumoto <lucaswyassumoto@gmail.com>
2026-04-28 19:41:52 -0400

    commit adjustments

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,6 @@
 document.getElementById("btnmenu").addEventListener("click", function() {
     document.querySelector(".items").classList.toggle("active");
+    
+    const rotate = document.querySelector(".menu");
+    rotate.classList.toggle("active");
 });
