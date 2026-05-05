334576b49139fcbe430e3d564b592f604575a519
lucaswyassumoto <lucaswyassumoto@gmail.com>
2026-05-04 20:05:54 -0400

    js login
    ajuste de js para alternar entre login e cadastro

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,1 +1,21 @@
+const btnlogin = document.getElementById('btnlogin');
+const btncadastro = document.getElementById('btncadastro');
 
+const formlogin = document.getElementById('formlogin');
+const formcadastro = document.getElementById('formcadastro');
+
+btnlogin.addEventListener("click", function() {
+    formlogin.style.display = "flex";
+    formcadastro.style.display = "none";
+
+    btnlogin.classList.add("active");
+    btncadastro.classList.remove("active");
+});
+
+btncadastro.addEventListener("click", function() {
+    formlogin.style.display = "none";
+    formcadastro.style.display = "flex";
+
+    btnlogin.classList.remove("active");
+    btncadastro.classList.add("active");
+});
